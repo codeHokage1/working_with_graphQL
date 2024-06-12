62fe3efd02b9d681c25efbc93aaae8c38709ec3b
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-06-12 11:54:15 +0100

    Setup first graphql project

---
+++ project_1/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    reviews: () => db.reviews,
+    games: () => db.games,
+    authors: () => db.authors,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4004,
+  },
+});
+
+console.log(`🚀 Server ready at ${url}`);
