94f070db397d78963446a064c368c44f71ce11fc
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-06-14 11:21:14 +0100

    Set up logic for fetching linked data

--- project_1/index.js
+++ project_1/index.js
@@ -7,9 +7,22 @@ import db from "./_db.js";
 const resolvers = {
   Query: {
     reviews: () => db.reviews,
+    review: (parent, { id }) => db.reviews.find((review) => review.id === id),
     games: () => db.games,
+    game: (parent, { id }) => db.games.find((game) => game.id === id),
     authors: () => db.authors,
+    author: (parent, { id }) => db.authors.find((author) => author.id === id)
   },
+  Game: {
+    reviews: (parent) => db.reviews.filter((review) => review.game_id === parent.id)
+  },
+  Author: {
+    reviews: (parent) => db.reviews.filter((review) => review.author_id === parent.id)
+  },
+  Review: {
+    game: (parent) => db.games.find((game) => game.id === parent.game_id),
+    author: (parent) => db.authors.find((author) => author.id === parent.author_id)
+  }
 };
 
 const server = new ApolloServer({
